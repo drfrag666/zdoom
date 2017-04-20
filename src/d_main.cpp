4e7a92f242192362baed78616675e1e49ab6215c
alexey.lysiuk <alexey.lysiuk@gmail.com>
2017-04-20 16:20:51 +0300

    Final touch in fixing restart CCMD
    https://mantis.zdoom.org/view.php?id=595

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -2726,6 +2726,8 @@ void D_DoomMain (void)
 			*(afunc->VMPointer) = NULL;
 		}
 
+		GC::DelSoftRootHead();
+
 		PClass::StaticShutdown();
 
 		GC::FullGC();					// perform one final garbage collection after shutdown
