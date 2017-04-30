28e6c1bc07777336619ae934fdeaf3221c2be3ea
alexey.lysiuk <alexey.lysiuk@gmail.com>
2017-04-30 16:35:28 +0300

    Fixed map name in demos recorded with * argument
    https://mantis.zdoom.org/view.php?id=642

--- src/g_level.cpp
+++ src/g_level.cpp
@@ -253,7 +253,7 @@ CCMD(recordmap)
 				G_DeferedInitNew(mapname);
 				gameaction = ga_recordgame;
 				newdemoname = argv[1];
-				newdemomap = argv[2];
+				newdemomap = mapname;
 			}
 		}
 		catch (CRecoverableError &error)
