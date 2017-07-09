f08985e6f42c67ed0cc5e947a216d0c721f65eb5
Magnus Norddahl <dpjudas@users.noreply.github.com>
2017-07-09 15:12:31 +0200

    - Remember maximized window state

--- src/win32/hardware.cpp
+++ src/win32/hardware.cpp
@@ -55,6 +55,7 @@ EXTERN_CVAR (Float, vid_winscale)
 
 CVAR(Int, win_x, -1, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 CVAR(Int, win_y, -1, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
+CVAR(Bool, win_maximized, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 
 extern HWND Window;
 
@@ -282,6 +283,8 @@ void I_SaveWindowedPos ()
 			win_x = wrect.left;
 			win_y = wrect.top;
 		}
+
+		win_maximized = IsZoomed(Window) == TRUE;
 	}
 }
 
@@ -309,6 +312,9 @@ void I_RestoreWindowedPos ()
 		KeepWindowOnScreen (winx, winy, winw, winh, scrwidth, scrheight);
 	}
 	MoveWindow (Window, winx, winy, winw, winh, TRUE);
+
+	if (win_maximized && !Args->CheckParm("-0"))
+		ShowWindow(Window, SW_MAXIMIZE);
 }
 
 extern int NewWidth, NewHeight, NewBits, DisplayBits;
