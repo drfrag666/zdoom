9802bc35dd9a02ac71826ca45b4f2882ecad9aee
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-01-30 16:32:16 +0200

    Fixed resetting CVARs via internal menu CCMDs
    This restores functionality of reset to defaults/saved menu options

--- src/c_cvars.cpp
+++ src/c_cvars.cpp
@@ -156,11 +156,6 @@ void FBaseCVar::SetGenericRep (UCVarValue value, ECVarType type)
 	{
 		return;
 	}
-	else if (UnsafeExecutionContext && !(GetFlags() & CVAR_MOD))
-	{
-		Printf(TEXTCOLOR_RED "Cannot set console variable" TEXTCOLOR_GOLD " %s " TEXTCOLOR_RED "from unsafe command\n", GetName());
-		return;
-	}
 	else if ((Flags & CVAR_LATCH) && gamestate != GS_FULLCONSOLE && gamestate != GS_STARTUP)
 	{
 		FLatchedValue latch;
@@ -1703,11 +1698,25 @@ void C_ArchiveCVars (FConfigFile *f, uint32 filter)
 
 EXTERN_CVAR(Bool, sv_cheats);
 
+static bool IsUnsafe(const FBaseCVar *const var)
+{
+	const bool unsafe = UnsafeExecutionContext && !(var->GetFlags() & CVAR_MOD);
+	if (unsafe)
+	{
+		Printf(TEXTCOLOR_RED "Cannot set console variable" TEXTCOLOR_GOLD " %s " TEXTCOLOR_RED "from unsafe command\n", var->GetName());
+	}
+	return unsafe;
+}
+
 void FBaseCVar::CmdSet (const char *newval)
 {
 	if ((GetFlags() & CVAR_CHEAT) && !sv_cheats)
 	{
 		Printf("sv_cheats must be true to set this console variable.\n");
+		return;
+	}
+	else if (IsUnsafe(this))
+	{
 		return;
 	}
 
@@ -1796,6 +1805,11 @@ CCMD (toggle)
 	{
 		if ( (var = FindCVar (argv[1], &prev)) )
 		{
+			if (IsUnsafe(var))
+			{
+				return;
+			}
+
 			val = var->GetGenericRep (CVAR_Bool);
 			val.Bool = !val.Bool;
 			var->SetGenericRep (val, CVAR_Bool);
