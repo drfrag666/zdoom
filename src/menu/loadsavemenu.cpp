c905cdcfc1eaa93ef7987662aa2c86fa2079936b
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-08-11 18:37:32 +0200

    - added checks for the special hacky quicksave marker.
    # Conflicts:
    #	src/menu/loadsavemenu.cpp

--- src/menu/loadsavemenu.cpp
+++ src/menu/loadsavemenu.cpp
@@ -352,7 +352,7 @@ void FSavegameManager::NotifyNewSave(const FString &file, const FString &title, 
 			node->bMissingWads = false;
 			if (okForQuicksave)
 			{
-				if (quickSaveSlot == nullptr) quickSaveSlot = node;
+				if (quickSaveSlot == nullptr || quickSaveSlot == (FSaveGameNode*)1) quickSaveSlot = node;
 				LastAccessed = LastSaved = i;
 			}
 			return;
@@ -368,7 +368,7 @@ void FSavegameManager::NotifyNewSave(const FString &file, const FString &title, 
 
 	if (okForQuicksave)
 	{
-		if (quickSaveSlot == nullptr) quickSaveSlot = node;
+		if (quickSaveSlot == nullptr || quickSaveSlot == (FSaveGameNode*)1) quickSaveSlot = node;
 		LastAccessed = LastSaved = index;
 	}
 }
