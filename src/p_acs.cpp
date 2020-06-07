f06f10dbc20e669c6649ca756f7e05833ca13fdc
alexey.lysiuk <alexey.lysiuk@gmail.com>
2020-06-07 12:16:46 +0300

    - fixed (un)morphing of more than one actor from ACS
    https://forum.zdoom.org/viewtopic.php?t=68847
    
    # Conflicts:
    #	src/p_acs.cpp

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -10141,9 +10141,14 @@ scriptwait:
 				else
 				{
 					FActorIterator iterator (tag);
-					AActor *actor;
+					TArray<AActor*> actorsToMorph;
 
-					while ( (actor = iterator.Next ()) )
+					while (AActor *actor = iterator.Next())
+					{
+						actorsToMorph.Push(actor);
+					}
+
+					for (AActor *actor : actorsToMorph)
 					{
 						if (actor->player)
 						{
@@ -10192,9 +10197,14 @@ scriptwait:
 				else
 				{
 					FActorIterator iterator (tag);
-					AActor *actor;
+					TArray<AActor*> actorsToUnmorph;
 
-					while ( (actor = iterator.Next ()) )
+					while (AActor *actor = iterator.Next())
+					{
+						actorsToUnmorph.Push(actor);
+					}
+
+					for (AActor *actor : actorsToUnmorph)
 					{
 						if (actor->player)
 						{
